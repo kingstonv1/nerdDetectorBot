9acf041734bccb73ddd71e81ef50faa6fcb58ddd
kingstonv1 <kingstongise@gmail.com>
2022-09-14 13:36:56 -0400

    Added nerd-detecting capabilities.

--- test.js
+++ test.js
@@ -8,13 +8,12 @@ const EventEmitter = require('events');
 const { rawListeners } = require('process');
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 const rest = new REST({ version: '10'}).setToken(keys.token);
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const readline = require('readline').createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
     });
 
-EventEmitter.setMaxListeners(14);
 
 const commands =
 [
@@ -46,15 +45,14 @@ async function getCompliment(url)
         console.log('Successfully reloaded application slash commands.');
     }
     catch (error) 
-    {
-        console.error(error);
-    }
+        { console.error(error); }
 })();
 
 client.on('ready', () => 
 {
     console.log(`Logged in as ${client.user.tag}!`);
-})
+    client.user.setPresence({ activities: [{ type: "Detecting", name: "nerds..."}], status: "online" });
+});
 
 client.on('interactionCreate', async interaction => 
 {
@@ -66,6 +64,15 @@ client.on('interactionCreate', async interaction =>
     else if (interaction.commandName === 'compliment')
     {
         
+    }
+});
+
+client.on('messageCreate', async message => 
+{
+    //If a message is sent by Isaac (currently me)
+    if (message.author.id === '546827180404113426') 
+    {
+        message.react('\:nerding\:');
     }
 });
 
